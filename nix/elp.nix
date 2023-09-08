4778033aba0fe031e70764e80745ddf34bbc797b
chiroptical <chiroptical@gmail.com>
2023-09-07 20:38:09 -0400

    Branch on system for elp

--- nix/elp.nix
+++ nix/elp.nix
@@ -1,17 +1,26 @@
-{ pkgs, ... }:
-
+{
+  pkgs,
+  system,
+  ...
+}:
 pkgs.stdenv.mkDerivation {
   name = "elp";
 
-  src = pkgs.fetchurl {
-    url = "https://github.com/WhatsApp/erlang-language-platform/releases/download/2023-09-07_1/elp-linux-otp-26.tar.gz";
-    sha256 = "sha256-yiSyP1N17Dkh0tDnGumE6RyIXBtNGhikF8G1kqOJf/Q";
-  };
-
-  phases = [ "installPhase" ];
+  src =
+    if builtins.elem system ["aarch64-darwin" "x86_64-darwin"]
+    then
+      pkgs.fetchzip {
+        url = "https://github.com/WhatsApp/erlang-language-platform/releases/download/2023-09-07_1/elp-macos-otp-25.3.tar.gz";
+        sha256 = "sha256-JVnd867lYc7mQqfYYiCOEI+nFPiUXrpEf0awRFVysW0=";
+      }
+    else # x86_64-linux
+      pkgs.fetchzip {
+        url = "https://github.com/WhatsApp/erlang-language-platform/releases/download/2023-09-07_1/elp-linux-otp-26.tar.gz";
+        sha256 = "sha256-VuX4Pwhw5NDO7QdmMXOYS4C2Sb51TcGhtU+AtKC+POo=";
+      };
 
   installPhase = ''
     mkdir -p $out/bin
-    tar xvf $src -C $out/bin
+    cp $src/elp $out/bin
   '';
 }
