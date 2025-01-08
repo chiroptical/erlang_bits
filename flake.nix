ab4bfdbb12588329e935d89a09a01fb0a523c242
chiroptical <chiroptical@gmail.com>
2025-01-08 17:30:06 -0500

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,27 +3,26 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"] (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowBroken = true;
-      };
-      # erlang_bits = pkgs.callPackage ./erlang_bits.nix {};
-    in {
-      devShell = import ./shell.nix {
-        inherit pkgs;
-      };
-      # defaultPackage = erlang_bits;
-      packages = flake-utils.lib.flattenTree {
-        # inherit erlang_bits;
-      };
-    });
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs supportedSystems (
+        system: function nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShell = forAllSystems (
+      pkgs:
+        import ./shell.nix {
+          inherit pkgs;
+        }
+    );
+  };
 }
